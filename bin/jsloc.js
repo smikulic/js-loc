0754a088c11964019ffc70e9ebe6c5cb7f601f44
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2018-02-27 21:12:01 +0100

    Count all nested folders and files

--- bin/jsloc.js
+++ bin/jsloc.js
@@ -1,19 +1,47 @@
 #!/usr/bin/env node
-var fs = require("fs");
-var path = require("path");
-var normalizedPath = path.join(process.cwd(), "/");
+let fs = require("fs");
+let readline = require('readline');
+let path = require("path");
+let normalizedPath = path.join(process.cwd(), "/");
 
-// Stats
-var totalLines = 0;
-var totalVariables = 0;
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-console.log("Start counting the lines...");
+// Stats result
+let totalLines = 0;
+let totalVariables = 0;
+let allFiles = [];
 
-fs.readdirSync(normalizedPath).forEach(function(file) {
+// Start
+rl.write('Counting.');
 
+const walkSync = (dir, filelist = []) => {
+  fs.readdirSync(dir).forEach(file => {
+    
+    // TODO: add blacklisting instead just git folder
+    if (file !== '.git') {
+      filelist = fs.statSync(path.join(dir, file)).isDirectory()
+        ? walkSync(path.join(dir, file), filelist)
+        : filelist.concat(path.join(dir, file));  
+    }
+
+  });
+
+return filelist;
+}
+
+// Loop through all nested folders and gather files
+allFiles = walkSync(normalizedPath);
+
+// Loop through all valid files to get the count
+allFiles.forEach(function(file) {
   // Match only javascript files
   if (file.match(/\.js$/)) {
-    var fileContent = fs.readFileSync(file).toString();
+    let fileContent = fs.readFileSync(file).toString();
+    // Display progress while going through files
+    rl.write('.');
     // Match variables
     totalVariables += fileContent.match(/var /g) ?
       fileContent.match(/var /g).length : 0;
@@ -22,5 +50,10 @@ fs.readdirSync(normalizedPath).forEach(function(file) {
   }
 });
 
-console.log("Total number of lines: ", totalLines);
-console.log("Total number of variables: ", totalVariables);
+rl.write("\n\nStats");
+rl.write("\n_____________________________________________");
+rl.write(`\nTotal LOC:         | ${totalLines}`);
+rl.write(`\nTotal variables:   | ${totalVariables}`);
+rl.write("\n_____________________________________________");
+rl.write("\n");
+rl.close();
